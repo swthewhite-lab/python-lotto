deb4933d4adee45a6a4cb6d18ef15f18414303bd
백승우 <swthewhite@gmail.com>
2024-12-30 19:21:38 +0900

    [#0] improve Lotto class initialization and validation method

--- src/lotto/lotto.py
+++ src/lotto/lotto.py
@@ -1,11 +1,12 @@
+from typing import List
+
 class Lotto:
-    def __init__(self, numbers):
-        self.validate(numbers)
+    def __init__(self, numbers: List[int]):
+        self._validate(numbers)
         self._numbers = numbers
 
-    def validate(self, numbers):
+    def _validate(self, numbers: List[int]):
         if len(numbers) != 6:
-            raise ValueError("로또 번호는 6개의 숫자여야 합니다.")
-        # TODO: 추가 유효성 검사 구현
+            raise ValueError
 
     # TODO: 추가 기능 구현
