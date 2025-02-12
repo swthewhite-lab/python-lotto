70da19805020234852fb65456cd5fb58b885996a
chaerishme <hana@hanaui-noteubug.local>
2025-02-12 16:02:00 +0900

    feat: 로또 발행 기능 추가
    사용자에게 금액 입력 받아 유효성 검사한 뒤 로또 발행하여 출력

--- src/lotto/main.py
+++ src/lotto/main.py
@@ -1,5 +1,38 @@
+import random
+
+
 def main():
-    # TODO: 프로그램 구현
+    count = check_valid()
+    lotto_list = make_lotto(count)
+    print_list(count, lotto_list)
+
+    user_num = num_list()
+
+
+def check_valid():
+    while True:
+        print("구입금액을 입력해 주세요.")
+        price = int(input())
+        try:
+            if price % 1000 == 0 :
+                return price // 1000
+            raise ValueError
+        except ValueError:
+            print("[ERROR] 구입 금액은 1,000원으로 나누어 떨어져야 합니다.")
+
+
+def make_lotto(count):
+    lotto_list = [sorted(random.sample(range(1,46),6)) for _ in range(count)]
+    return lotto_list
+
+
+def print_list(count, lotto_list):
+    print()
+    print(f"{count}개를 구매했습니다.")
+    for lotto in lotto_list:
+        print(lotto)
+
+def num_list():
     pass
 
 if __name__ == "__main__":
