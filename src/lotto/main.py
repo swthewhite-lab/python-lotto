a9cd6868bfb8ea8a60e9e0b400cc8a0044faf419
akran <nhlk123@naver.com>
2025-02-13 19:34:41 +0900

    feat: 예외처리 구현

--- src/lotto/main.py
+++ src/lotto/main.py
@@ -1,9 +1,17 @@
 def purchase_price_Input () :
-    purchase_price=int(input("구입금액을 입력해 주세요."))
+    purchase_price=input("구입금액을 입력해 주세요.")
+    if not purchase_price.isdigit():
+        raise ValueError("[ERROR] 숫자를 입력해야 합니다.")
+    
+    purchase_price=int(purchase_price)
+    
+    if purchase_price %1000 !=0:
+        raise ValueError("[ERROR] 1000원 단위로 입력해야 합니다.")
+    
     return purchase_price
 
 def main():
-    a=purchase_price_Input()
-    print(a)
+    purchase_price_Input()
+    
 if __name__ == "__main__":
     main()
