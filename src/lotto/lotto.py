96ff6507a2e1956f1e8c64f518f12f6ae7bdecc6
chaerishme <hana@hanaui-noteubug.local>
2025-02-13 18:07:31 +0900

    feat: 사용자 입력 유효성 검사 및 로또 생성 기능 추가
    main.py에서 이전

--- src/lotto/lotto.py
+++ src/lotto/lotto.py
@@ -1,12 +1,27 @@
 from typing import List
+import random
 
 class Lotto:
     def __init__(self, numbers: List[int]):
         self._validate(numbers)
-        self._numbers = numbers
+        self._numbers = sorted(numbers)
 
     def _validate(self, numbers: List[int]):
         if len(numbers) != 6:
-            raise ValueError
+            raise ValueError("로또 번호는 6개여야 합니다.")
+        if len(set(numbers)) < 6:
+            raise ValueError("로또 번호는 중복되어서는 안됩니다.")
+        if max(numbers) > 46 or min(numbers) < 1:
+            raise ValueError("로또 번호의 숫자 범위는 1~45까지입니다.")
 
-    # TODO: 추가 기능 구현
+
+    @classmethod
+    def generate_num(cls):
+        return cls(random.sample(range(1,46),6))
+    
+
+    def get_numbers(self):
+        return self._numbers
+    
+    def __str__(self):
+        return str(self._numbers)
--- src/lotto/main.py
+++ src/lotto/main.py
@@ -1,10 +1,10 @@
 import random
-
+from lotto import Lotto
 
 def main():
     count = check_valid()
-    lotto_list = make_lotto(count)
-    print_lotto(count, lotto_list)
+    lotto_list = [Lotto.generate_num() for _ in range(count)]
+    print_lotto(lotto_list)
 
     user_num = user_input()
     bonus_num = bonus_input(user_num)
@@ -21,13 +21,8 @@ def check_valid():
             print("[ERROR] 구입 금액은 1,000원으로 나누어 떨어져야 합니다.")
 
 
-def make_lotto(count):
-    lotto_list = [sorted(random.sample(range(1,46),6)) for _ in range(count)]
-    return lotto_list
-
-
-def print_lotto(count, lotto_list):
-    print(f"\n{count}개를 구매했습니다.")
+def print_lotto(lotto_list):
+    print(f"\n{len(lotto_list)}개를 구매했습니다.")
     for lotto in lotto_list:
         print(lotto)
 
@@ -35,34 +30,31 @@ def print_lotto(count, lotto_list):
 def user_input():
     while True:
         print("\n당첨 번호를 입력해 주세요.")
-        user_num = list(map(int, input().split(',')))
         try:
-            if len(set(user_num)) < 6:
-                raise ValueError("숫자는 중복되지 않아야 합니다.")
-            if len(user_num) > 6:
-                raise ValueError("숫자 6개를 입력해주세요.")
-            if max(user_num) > 46 or min(user_num) < 1:
-                raise ValueError("로또 번호의 숫자 범위는 1~45까지입니다.")
-            return user_num
+            user_num = list(map(int, input().split(',')))
+            return Lotto(user_num).get_numbers()
         except ValueError as e:
             print(f"[ERROR] {e}")
 
 
 def bonus_input(user_num):
     while True:
-        print("\n보너스 번호를 입력해 주세요.")
-        bonus_num = input()
         try: 
-            if not bonus_num.isdigit:
-                raise ValueError("숫자를 입력해 주세요.")
-            if int(bonus_num) in user_num:
-                raise ValueError("보너스 숫자와 입력한 당첨 번호는 중복되지 않아야 합니다.")
-            if int(bonus_num) > 46 or int(bonus_num) < 1:
-                raise ValueError("로또 번호의 숫자 범위는 1~45까지입니다.")
-            return int(bonus_num)
+            bonus_num = input("\n보너스 번호를 입력해 주세요.\n")
+            return validate_bonus(bonus_num, user_num)
         except ValueError as e:
             print(f"[ERROR] {e}") 
 
+
+def validate_bonus(bonus_num, user_num):
+    if not bonus_num.isdigit:
+            raise ValueError("숫자를 입력해 주세요.")
+    if int(bonus_num) in user_num:
+        raise ValueError("보너스 숫자와 입력한 당첨 번호는 중복되지 않아야 합니다.")
+    if int(bonus_num) > 46 or int(bonus_num) < 1:
+        raise ValueError("로또 번호의 숫자 범위는 1~45까지입니다.")
+
+    return bonus_num
 
 if __name__ == "__main__":
     main()
