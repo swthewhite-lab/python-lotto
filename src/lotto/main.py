a241007a8cf62d37661cfa77a252e4009c94a748
akran <nhlk123@naver.com>
2025-02-13 19:29:39 +0900

    feat: 구입 금액 입력 구현

--- src/lotto/main.py
+++ src/lotto/main.py
@@ -1,6 +1,9 @@
-def main():
-    # TODO: 프로그램 구현
-    pass
+def purchase_price_Input () :
+    purchase_price=int(input("구입금액을 입력해 주세요."))
+    return purchase_price
 
+def main():
+    a=purchase_price_Input()
+    print(a)
 if __name__ == "__main__":
     main()
