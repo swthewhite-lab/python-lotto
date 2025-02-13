b62a7c7a4821ebfe7e9f7e1e71b2806b56dc1760
chaerishme <hana@hanaui-noteubug.local>
2025-02-13 18:23:39 +0900

    feat: 당첨 개수와 보너스 여부로 등수 계산 기능 추가
    등수 계산하는 기능 추가

--- src/lotto/lotto.py
+++ src/lotto/lotto.py
@@ -1,4 +1,5 @@
 from typing import List
+from enum import Enum
 import random
 
 class Lotto:
@@ -14,14 +15,33 @@ class Lotto:
         if max(numbers) > 46 or min(numbers) < 1:
             raise ValueError("로또 번호의 숫자 범위는 1~45까지입니다.")
 
-
     @classmethod
     def generate_num(cls):
         return cls(random.sample(range(1,46),6))
-    
 
     def get_numbers(self):
         return self._numbers
     
     def __str__(self):
         return str(self._numbers)
+    
+
+class Rank(Enum):
+    FIRST = (6, False, 2_000_000_000)
+    SECOND = (5, True, 30_000_000)
+    THIRD = (5, False, 1_500_000)
+    FOURTH = (4, False, 50_000)
+    FIFTH = (3, False, 5_000)
+    NONE = (0, False, 0)
+
+    def __init__(self, match_cnt, bonus_match, prize):
+        self.match_cnt = match_cnt
+        self.bonus_match = bonus_match
+        self.prize = prize
+
+    @classmethod
+    def get_rank(cls, match_cnt, bonus):
+        for rank in cls:
+            if rank.match_cnt == match_cnt and rank.bonus_match == bonus:
+                return rank
+        return cls.NONE
