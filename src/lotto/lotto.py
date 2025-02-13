df8e25951885299ff09543a767d3dd5fcd11544f
AI-WonYJ <yjason7587@naver.com>
2025-02-14 02:35:50 +0900

    feat: Add a function to generate numbers if lottery tickets
    구입한 로또 개수만큼 로또를 발행하는 함수 추가
    PEP8 Style로 변경
    기존 코드 개선 및 주석 추가가

--- src/lotto/lotto.py
+++ src/lotto/lotto.py
@@ -1,12 +1,30 @@
 from typing import List
+import random
 
 class Lotto:
-    def __init__(self, numbers: List[int]):
+    def __init__(self, numbers: List[int] = None):
+        if numbers is None:  # numbers가 주어지지 않으면 자동으로 생성성
+            numbers = self.issuance_lotto()
         self._validate(numbers)
         self._numbers = numbers
 
+
     def _validate(self, numbers: List[int]):
         if len(numbers) != 6:
-            raise ValueError
+            raise ValueError()
 
-    # TODO: 추가 기능 구현
+    @staticmethod
+    def issuance_lotto():
+        """랜덤한 6자리 로또 번호 생성 후 정렬하여 반환"""
+        value = sorted(random.sample(range(1, 46), 6))
+        return value
+    
+
+    def __str__(self):
+        """str 형식으로 변환하여 반환"""
+        return str(self._numbers)
+    
+
+    def get_numbers(self):
+        """로또 번호 리스트 반환환"""
+        return self._numbers
--- src/lotto/main.py
+++ src/lotto/main.py
@@ -1,3 +1,6 @@
+from lotto import Lotto
+
+
 LOTTO_EACH_PRICE = 1000  # 로또 구입 금액 단위 매직넘버상수
 
 
@@ -21,22 +24,27 @@ def validate_input_purchase_amount(data):
 
 def input_purchase_amount():  # 구입 금액 입력 함수
     print("구입금액을 입력해 주세요.")
-    while True:
-        purchase_amount = input()
-        purchase_amount = validate_input_purchase_amount(purchase_amount)  # 구입 금액 검증
-        return purchase_amount
+    purchase_amount = input()
+    purchase_amount = validate_input_purchase_amount(purchase_amount)  # 구입 금액 검증
+    return purchase_amount
 
 
+# 구매 가능한 로또 개수 계산산
 def generate_lotto_quantity(purcahse_amount):
     lotto_quantity = purcahse_amount // LOTTO_EACH_PRICE
     print("\n{0}개를 구매했습니다.".format(lotto_quantity))
     return lotto_quantity
 
 
+
+
 def main():
     purchase_amount = input_purchase_amount()  # 구입 금액 입력
-    lotto_quantity = generate_lotto_quantity(purchase_amount)
+    lotto_quantity = generate_lotto_quantity(purchase_amount)  # 로또 수량 계산산
 
+    issued_lotto_list = [Lotto.issuance_lotto() for _ in range(lotto_quantity)]  # 로또 발행
+    for lotto in issued_lotto_list:  # 발행된 로또 출력
+        print(lotto)
 
 
 if __name__ == "__main__":
