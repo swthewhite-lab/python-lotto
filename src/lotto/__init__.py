8e7d8cc9cd7c5e4193f72da9e10e1e7a849f7492
백승우 <swthewhite@gmail.com>
2025-02-13 19:59:16 +0900

    [#0] add initial module imports for lotto package & guide about importing packages

---
+++ src/lotto/__init__.py
@@ -0,0 +1,7 @@
+# src/lotto/__init__.py
+
+# 이 패키지를 외부에서 사용할 때 필요한 모듈을 여기에 import합니다.
+# 새로운 모듈을 추가할 경우 아래에 from .[모듈명] import [클래스/함수] 형식으로 추가하세요.
+
+from .lotto import Lotto  # 로또 클래스
+# from .other_module import OtherClass  # 예: 다른 모듈 추가 시
